c34e509b65b9957cdd2f2ce7a90ecaa1baa2759e
shilz-123 <122595633+shilz-123@users.noreply.github.com>
2023-02-27 16:53:30 +0530

    Add files via upload

---
+++ src/helper/data.js
@@ -0,0 +1,68 @@
+export const data = [
+    {
+      id: 1,
+      title: "New York",
+      desc: "Home to the Statue of Liberty, Empire State Building, Central Park, Times Square, New York is the most populous city in the USA. We have a lot to tell about the city where city lights illuminate the sky.",
+      image:
+        "newyork.jpg",
+    },
+    {
+      id: 2,
+      title: "Philippines",
+      desc: "The island country, the Philippines, is one of the most beautiful countries where blue and green are together. Forget the potion of youth, traveling to the Philippines will rejuvenate you for at least 5 years.",
+      image:
+        "philipines.jpg",
+    },
+    {
+      id: 3,
+      title: "Bali",
+      desc: "Bali, Indonesia's most famous island, has taken its place as the 'last paradise' in mind. Providing 80% of its livelihood with tourism, Bali hosts thousands of tourists every year.",
+      image: "bali.webp",
+    },
+    {
+      id: 4,
+      title: "Egypt",
+      desc: "Being one of the biggest countries of the Middle East, Egypt has been the transition point of many civilizations with its thousands of years of history. The pyramids and miracles that come to mind when talking about Egypt have been the subject of many films. It still remains a mystery.",
+      image:
+        "egyt.jpg",
+    },
+    {
+      id: 5,
+      title: "Cappadocia",
+      desc: "Cappadocia… 'Kappa Tuchia', which means 'Land of Beautiful Horses' in the words of Persians. A location in Turkey and in the center of Anatolia's most popular and unique tourist centers of someone who Cappadocia, magnificent nature, valleys of unique beauty, fairy chimneys, rock-hewn settlements, millions every year with open-air museums and underground cities, local and hosts foreign tourists.",
+      image: "turkey.webp",
+    },
+    {
+      id: 6,
+      title: "Paris",
+      desc: "Paris has been recognized as one of the most romantic cities in the world. Many famous buildings here have been added to the Unesco World Heritage List. It awaits you with its museums, fashion and historical buildings.",
+      image:
+        "paris.jpg",
+    },
+    {
+      id: 7,
+      title: "India",
+      desc: "If you are not a vacationer but a traveler, we can say that India is the place for you. India, one of the most colorful places in the world, has a crazy variety. The Taj Mahal, the story of which you have heard more or less, is one of its famous buildings.",
+      image: "india.jpg",
+    },
+    {
+      id: 8,
+      title: "Northern Lights",
+      desc: "Northern lights, the 'Aurora Borealis', named after the Dawn Goddess Aurora, are the natural glows that emerge in the sky with the interaction of the earth's magnetic field and charged particles from the Sun. This image, which makes most of us feel like in a fairy tale land, deserves to be visited at least once in a lifetime.",
+      image:
+        "northern lights.jpg",
+    },
+    {
+      id: 9,
+      title: "Fuji Mountain",
+      desc: "The symbol and highest mountain of Japan, Mount Fuji is considered sacred by the Japanese and is called Fuji-San. This mountain, which has an active startovolcano, is depicted in art and photography and is a natural wonder visited by athletes and tourists.",
+      image:
+        "fuji mountain.jpg",
+    },
+    {
+      id: 10,
+      title: "Arashiyama Bamboo Grove",
+      desc: "Bamboo Forest, or Arashiyama Bamboo Grove or Sagano Bamboo Forest, is a natural forest of bamboo in Arashiyama, Kyoto, Japan. The forest consists mostly of Moso Bamboo (Phyllostachys edulis) and consists of several pathways for tourists and visitors. The Ministry of the Environment considers it a part of the soundscape of Japan.",
+      image: "bamboos.jpg",
+    },
+];
